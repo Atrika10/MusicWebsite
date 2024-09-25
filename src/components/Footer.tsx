9a3adfd46419b56dba780759a15996bf2636372d
ATRIKA SHOW <readeratrika08@gmail.com>
2024-09-25 11:08:24 +0530

    Refactor Footer component: Update grid layout and alignment for responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className=' text-white grid grid-cols-4 justify-center px-10 py-24 gap-2 dark:bg-grid-blue-500/[0.15] bg-grid-black/[0.2]  shadow-[inset_0px_20px_84px_29px_#000000]'>
+        <div className=' text-white px-10 py-24  dark:bg-grid-blue-500/[0.15] bg-grid-black/[0.2]  shadow-[inset_0px_20px_84px_29px_#000000] grid justify-center grid-cols-1
+        gap-10
+        sm:grid-cols-2  sm:gap-20
+        md:grid-cols-4 md:gap-2'>
             
-            <div>
+            <div className='flex flex-col items-center sm:items-start'>
                 <h2 className='py-3'>About us</h2>
                 <p className='text-neutral-400'>We are dedicated to nurturing musical talent and providing a platform for artists to grow and shine. Join us on this melodious journey!</p>
             </div>
 
-            <div className='flex flex-col justify-center items-center'>
+            <div className='flex flex-col justify-center items-center sm:items-start md:items-center'>
                 <h2 className='py-3'>Quick Links</h2>
                 <ul className='text-neutral-400 cursor-pointer'>
                     <li>Home</li>
@@ -20,7 +23,7 @@ const Footer = () => {
                 </ul>
             </div>
 
-            <div className='flex flex-col justify-center items-center'>
+            <div className='flex flex-col justify-center items-center sm:items-start md:items-center'>
                 <h2 className='py-3'>Follow Us</h2>
                 <ul className=
                 'text-neutral-400 cursor-pointer'>
@@ -32,7 +35,7 @@ const Footer = () => {
                 </ul>
             </div>
 
-            <div>
+            <div className='flex flex-col justify-center items-center sm:items-start'>
                 <h2 className='py-3'>Contact Us</h2>
                 <div className='text-neutral-400 flex flex-col justify-center'>
                 <p>Email: info@musicwebsite.com</p>
